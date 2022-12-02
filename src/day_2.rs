4659048f9451a511865d93d3c2a30ce631faedd7
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-02 15:39:45 +0100

    Improve day 2 with lookup tables

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,73 +1,20 @@
 use std::{env::args, fs};
 
 pub fn main() {
-    println!(
-        "A: {}\nB: {}",
-        fs::read_to_string(args().collect::<Vec<String>>().get(1).unwrap())
-            .unwrap()
-            .replace('\r', "")
-            .split('\n')
-            .map(|it| {
-                let it = it.split(' ').collect::<Vec<_>>();
-                let a = it[0].chars().next().unwrap();
-                let b = it[1].chars().next().unwrap();
-                match a {
-                    'A' => match b {
-                        'X' => return 1 + 3,
-                        'Y' => return 2 + 6,
-                        'Z' => return 3 + 0,
-                        _ => unreachable!(),
-                    },
-                    'B' => match b {
-                        'X' => return 1 + 0,
-                        'Y' => return 2 + 3,
-                        'Z' => return 3 + 6,
-                        _ => unreachable!(),
-                    },
-                    'C' => match b {
-                        'X' => return 1 + 6,
-                        'Y' => return 2 + 0,
-                        'Z' => return 3 + 3,
-                        _ => unreachable!(),
-                    },
-                    _ => unreachable!(),
-                }
-            })
-            .collect::<Vec<u32>>()
-            .iter()
-            .sum::<u32>(),
-        fs::read_to_string(args().collect::<Vec<String>>().get(1).unwrap())
-            .unwrap()
-            .replace('\r', "")
-            .split('\n')
-            .map(|it| {
-                let it = it.split(' ').collect::<Vec<_>>();
-                let a = it[0].chars().next().unwrap();
-                let b = it[1].chars().next().unwrap();
-                match a {
-                    'A' => match b {
-                        'X' => return 3 + 0,
-                        'Y' => return 1 + 3,
-                        'Z' => return 2 + 6,
-                        _ => unreachable!(),
-                    },
-                    'B' => match b {
-                        'X' => return 1 + 0,
-                        'Y' => return 2 + 3,
-                        'Z' => return 3 + 6,
-                        _ => unreachable!(),
-                    },
-                    'C' => match b {
-                        'X' => return 2 + 0,
-                        'Y' => return 3 + 3,
-                        'Z' => return 1 + 6,
-                        _ => unreachable!(),
-                    },
-                    _ => unreachable!(),
-                }
-            })
-            .collect::<Vec<u32>>()
-            .iter()
-            .sum::<u32>()
-    );
+    let mut ar = 0;
+    let mut br = 0;
+    let a_lookup = [[4, 8, 3], [1, 5, 9], [7, 2, 6]];
+    let b_lookup = [[3, 4, 8], [1, 5, 9], [2, 6, 7]];
+    fs::read_to_string(args().collect::<Vec<String>>().get(1).unwrap())
+        .unwrap()
+        .replace('\r', "")
+        .split('\n')
+        .for_each(|it| {
+            let it = it.split(' ').collect::<Vec<_>>();
+            let a = it[0].chars().next().unwrap() as u32 - 'A' as u32;
+            let b = it[1].chars().next().unwrap() as u32 - 'X' as u32;
+            ar += a_lookup[a as usize][b as usize];
+            br += b_lookup[a as usize][b as usize];
+        });
+    println!("A: {}\nB: {}", ar, br);
 }
