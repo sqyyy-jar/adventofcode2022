f6004884d70d44a6023fd766661cdf2b0187b0bb
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-03 10:40:32 +0100

    Improve day one

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,32 +1,21 @@
 use std::{env::args, fs};
 
 pub fn main() {
+    let text = fs::read_to_string(args().collect::<Vec<String>>().get(1).unwrap())
+        .unwrap()
+        .replace('\r', "");
+    let mut sums: Vec<_> = text
+        .split("\n\n")
+        .map(|it| {
+            it.split("\n")
+                .map(|it| it.parse::<i64>().unwrap())
+                .sum::<i64>()
+        })
+        .collect();
+    sums.sort();
     println!(
         "A: {}\nB: {}",
-        fs::read_to_string(args().collect::<Vec<String>>().get(1).unwrap())
-            .unwrap()
-            .replace('\r', "")
-            .split("\n\n")
-            .map(|it| {
-                it.split("\n")
-                    .map(|it| it.parse::<i64>().unwrap())
-                    .sum::<i64>()
-            })
-            .max()
-            .unwrap(),
-        {
-            let mut v = fs::read_to_string(args().collect::<Vec<String>>().get(1).unwrap())
-                .unwrap()
-                .replace('\r', "")
-                .split("\n\n")
-                .map(|it| {
-                    it.split("\n")
-                        .map(|it| it.parse::<i64>().unwrap())
-                        .sum::<i64>()
-                })
-                .collect::<Vec<i64>>();
-            v.sort();
-            v.drain(v.len() - 3..).sum::<i64>()
-        }
+        sums.iter().max().unwrap(),
+        sums.iter().rev().take(3).sum::<i64>(),
     );
 }
